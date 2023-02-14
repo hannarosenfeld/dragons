0a7e81027bd7bed2581a221fc3af6ccb950c6e0b
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-02-14 17:19:50 -0500

    Phase 2.

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,22 @@
-// Your code here
+class Dragon {
+  constructor(name , color) {
+    this.name = name;
+    this.color = color;
+  }
 
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    return dragons.map(dragon => dragon.name);
+  }
+}
+
+let test = new Dragon("puff", "pink");
+
+console.log(test)
+console.log(Dragon.getDragons([test]))
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
