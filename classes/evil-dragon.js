798aa29b774fd9db68743da64e11fa2b6ccf0ada
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-02-14 18:18:51 -0500

    Phase 3.

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,19 @@
-// Your code 
+const Dragon = require(`./dragon.js`)
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        this.evilDoings.forEach(doings => console.log(`${this.name} will ${doings}`))
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+    }
+}
+
+module.exports = EvilDragon;
